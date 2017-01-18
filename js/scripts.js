c83377da9ff17dbaf1956789a2ac707500d207d8
danlauby <dmlauby@gmail.com>
2017-01-18 14:38:45 -0800

    refactor spec 2 and 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 // Buisness logic
 var leapYear = function(year) {
-  if (year % 4 === 0) {
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
     return true;
-  } else {
+  }  else {
     return false;
   }
 
